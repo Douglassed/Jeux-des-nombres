a9b36374cffd6d3a93d8b23364c5a6b9bcf91a34
clallavena <allavenaclementcg@gmail.com>
2021-12-03 11:23:15 +0100

    Add dialog when forfeit, and confirmation, add Forfeit button

--- src/Game.vue
+++ src/Game.vue
@@ -27,13 +27,31 @@
       <!-- PlaceHolder -->
     </div>
     <div class="col">
-      <button class="btn btn-danger" type="button">Abandonner</button>
+      <button class="btn btn-danger" type="button" data-toggle="modal" data-target="#forfeitDialog">Abandonner</button>
     </div>
     <div class="col">
       <!-- PlaceHolder -->
     </div>
   </div>
 
+  <!-- Modal -->
+  <div class="modal fade" id="forfeitDialog" tabindex="-1" aria-labelledby="forfeitLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="forfeitLabel">Abandonner ?</h5>
+          <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          Etes vous sur de vouloir abandonner au bout de {{nbOfTry}}?
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+          <button type="button" class="btn btn-primary">Abandonner</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -42,6 +60,7 @@ export default {
   data() {
     return {
       FAKE_NUMBER_TO_GUESS: 100,
+      nbOfTry: 0,
       guessVar: 0,
       minutes: 10,
       seconds: 0,
@@ -76,8 +95,12 @@ export default {
           this.decrease()
       }.bind(this),1000)
     },
+    forfeit() {
+
+    },
     guess(){
-      this.showHint = true
+      this.showHint = true;
+      this.nbOfTry++;
       if (this.guessVar == this.FAKE_NUMBER_TO_GUESS){
         this.hint = "Bien joué"
         return
