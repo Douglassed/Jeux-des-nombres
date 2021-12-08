182eb62fd318240a73d84122c7d944d5200af59e
clallavena <allavenaclementcg@gmail.com>
2021-12-08 16:22:52 +0100

    Add an array of games into vuex store, with getters for stats

--- src/store.js
+++ src/store.js
@@ -2,10 +2,40 @@ import {createStore} from 'vuex'
 
 export const store = createStore({
     state() {
+        let averageElapsedTime;
         return {
-            todos: []
+            count: 0,
+            games: [{}], // game(numGame, nbOfTry, timeElapsed, victory)
+            averageElapsedTime,
+            averageNbOfTry: Number
         }
     },
-    getters: {},
-    mutations: {}
+    getters: {
+        getAllGames: state => {
+            return state.games
+        },
+        getAllGamesCount: state => {
+            return state.games.length
+        },
+        getAllWiningGames: state => {
+            return state.games.filter(game => game.victory === "true").length
+        },
+        getAllLosingGames: state => {
+            return state.games.filter(game => game.victory === "false").length
+        },
+        getAllTime: state => {
+            return state.games.filter(game => game.time)
+        },
+        getAllNbOfTry: state => {
+            return state.games.filter(game => game.nbOfTry)
+        }
+    },
+    mutations: {
+        ADD_GAME: (state, payload) => {
+            state.games.push({numGame: state.count, nbOfTry: payload.nbOfTry, timeElapsed: payload.timeElapsed, victory: payload.victory })
+        },
+        increment (state){
+            state.count++
+        }
+    }
 })
