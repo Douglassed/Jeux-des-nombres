73766a5bcd23a1404dbf595e76e61399e74d4416
clallavena <allavenaclementcg@gmail.com>
2021-12-20 21:22:47 +0100

    Add API REST communication with try and token, and fix some pendings bugs

--- src/store.js
+++ src/store.js
@@ -4,11 +4,13 @@ import game from "./component/Game";
 export const store = createStore({
     state() {
         let averageElapsedTime;
+        let token;
         return {
             count: 0,
             games: [], // game(numGame, nbOfTry, timeElapsed, victory)
             averageElapsedTime,
-            averageNbOfTry: Number
+            averageNbOfTry: Number,
+            token
         }
     },
     getters: {
@@ -31,15 +33,20 @@ export const store = createStore({
             const reducer = (previousValue, currentValue) => previousValue + currentValue;
             if (state.games.length === 0) return 0
             return state.games.map(game => game.nbOfTry).reduce(reducer)
+        },
+        getToken: state => {
+            return state.token
         }
     },
     mutations: {
         ADD_GAME: (state, payload) => {
-
             state.games.push({numGame: state.count, nbOfTry: payload.nbOfTry, timeElapsed: payload.timeElapsed, victory: payload.victory })
         },
         increment (state){
             state.count++
+        },
+        ADD_TOKEN: (state, payload) => {
+            state.token = payload.token
         }
     }
 })
